d99fb0d1fcfeaa8e7e1b41b3baf339d47c594dd9
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-17 14:37:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]\w\W\Z/)
+  if text.match(/\A[A-Z]\w{1,}\W\Z/)
     return true
   end
 end
