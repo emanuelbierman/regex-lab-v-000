94f84db0f7c2a4f46b469851abadaa54d46fdb4c
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-17 14:24:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z].*\W\Z/)
     return true
-  elsif text.match(//)
+  elsif text.match(/\A[a-z].*\W\Z/)
     return false
   end
 end
